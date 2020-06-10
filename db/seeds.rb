574b7216a6ccd34c17234f5a3f76e2fac7b17dd9
EC2 Default User <ec2-user@ip-172-31-45-218.ap-northeast-1.compute.internal>
2020-06-10 15:40:46 +0000

    database updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-User.create(email: 'taka.aquacoin@gmail.com', password: 'Basara0510', name: 'かんりにん', club_id: '31')
+User.create(email: 'taka.aquacoin@gmail.com', password: 'Basara0510', name: 'かんりにん', club_id: '31', admin: true)
