1843ee1b26a50369801665b1360832426ed8706d
EC2 Default User <ec2-user@ip-172-31-45-218.ap-northeast-1.compute.internal>
2020-06-10 15:20:50 +0000

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,1 @@
-sapporo = Club.find(1)
-sapporo.abbreviation = "札幌"
-sapporo.abbreviation = Club.where(abbreviation: sapporo.abbreviation).first_or_initialize
-sapporo.abbreviation.save
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: 'taka.aquacoin@gmail.com', password: 'Basara0510', name: 'かんりにん', club_id: '31')
